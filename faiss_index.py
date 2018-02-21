5e770dc117536505afc8c85ce01b85a6570e58e1
Arko Gupta <arkogupta16@gmail.com>
2018-02-21 21:01:31 +0530

    Update faiss_index.py

--- faiss_index.py
+++ faiss_index.py
@@ -7,17 +7,17 @@ import argparse
 """
 
 def build_index(feature_file):
-    file = h5py.File(feature_file,'r')
-    itms = file.items()
+    h = h5py.File(feature_file,'r')
+    itms = h.items()
     m = itms.__len__()
     #currently hardcoded     
-    n = 25088
-    xb = np.zeros((m,n))
+    dim = 25088
+    xb = np.zeros((m,dim))
     i = 0
     for imageName,features in itms:
         xb[i] = np.array(features)
         i += 1
     xb = np.float32(xb)
-    idx = faiss.IndexFlatL2(n)
+    idx = faiss.IndexFlatL2(dim)
     idx.add(xb)
     return idx
