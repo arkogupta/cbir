bb27d6af12f8c3d502912c1d93c91ce27f4da329
da230896 <da.230896@gmail.com>
2018-04-02 23:33:56 +0530

    Though PCA must not be in this but for now

--- faiss_index.py
+++ faiss_index.py
@@ -1,24 +1,41 @@
 import faiss
 import h5py
 import numpy as np
-import argparse
+from itertools import chain
+from PCA import apply_pca as PCA
+from feature_extractor import get_feature_file
 """
     creates a faiss index object
+    updated to create index without explicitely having dictionary in mem
+    --should increase speed--
 """
 
 
 def build_index(feature_file):
-    h = h5py.File(feature_file, 'r')
-    itms = h.items()
-    m = itms.__len__()
-    # currently hardcoded
-    dim = 25088
-    xb = np.zeros((m,dim))
-    i = 0
-    for imageName,features in itms:
-        xb[i] = np.array(features)
-        i += 1
-    xb = np.float32(xb)
+    xb = get_feature_file(feature_file)
+    m,dim = xb.shape
+
+    # reduced_dim = 6272
+    # xb = PCA(feature_file,reduced_dim)
+    
     idx = faiss.IndexFlatL2(dim)
     idx.add(xb)
-    return idx,xb
+    # return idx
+
+    # for performance tester
+    return xb,idx
+
+# For testing PCA unquote following code
+'''
+from datetime import datetime
+feature_file = '/home/dinesh/Documents/index_debug.hdf5'
+# feature_file = '/input/index_debug.hdf5'
+start = datetime.now()
+idx = build_index(feature_file)
+print("PCA working fine...")
+print("Time taken: %f secs" % (datetime.now() - start).total_seconds())
+
+# f = h5py.File('/output/pca_index.hdf5','w')
+with h5py.File('/home/dinesh/Documents/cbir/pca_index.hdf5','w') as f:
+    f.create_dataset("PCA on Ukbench",data = idx)
+'''
